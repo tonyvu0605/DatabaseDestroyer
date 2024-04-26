9624b9a074294efe54d0e38657364e45c1ea2fa2
tonyvu0605 <tonyvu0605@gmail.com>
2024-04-26 09:36:02 -0400

    fix(scss import) corrected the stylesheet import

--- client/src/sections/averagePlayerSalaries/AveragePlayerSalaries.jsx
+++ client/src/sections/averagePlayerSalaries/AveragePlayerSalaries.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAveragePlayerSalaries } from 'reduxes/averagePlayerSalariesSlice';
 
-import './averageplayersalaries.scss'; 
+import './averagePlayerSalaries.scss';
 
 const AveragePlayerSalariesView = () => {
   const dispatch = useDispatch();
