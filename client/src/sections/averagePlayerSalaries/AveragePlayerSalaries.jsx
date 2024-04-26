2d1230f1a31d65bc7a60b1304eb0206554f9d2c2
tonyvu0605 <tonyvu0605@gmail.com>
2024-04-26 10:07:40 -0400

    feat(home) implemented the home page

--- client/src/sections/averagePlayerSalaries/AveragePlayerSalaries.jsx
+++ client/src/sections/averagePlayerSalaries/AveragePlayerSalaries.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAveragePlayerSalaries } from 'reduxes/averagePlayerSalariesSlice';
 
-
 import './averagePlayerSalaries.scss';
 
 
--- client/src/sections/landing/LandingView.jsx
+++ client/src/sections/landing/LandingView.jsx
@@ -1,58 +1,98 @@
-import React, { useEffect } from 'react';
-import { fetch10Players } from 'reduxes/playerSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {
-  Table,
-  Paper,
-  TableRow,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableContainer,
+  Grid,
+  Card,
+  Container,
+  CardMedia,
+  Typography,
+  CardContent,
 } from '@mui/material';
 
 import './landingView.scss';
 
-const LandingView = () => {
-  const dispatch = useDispatch();
-
-  const playerData = useSelector((state) => state.player.playerData);
-
-  useEffect(() => {
-    dispatch(fetch10Players());
-  }, [dispatch]);
-
-  console.log(playerData);
-
-  return (
-    <div className="Landing">
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Player Name</TableCell>
-              <TableCell>Birthdate</TableCell>
-              <TableCell>School</TableCell>
-              <TableCell>Height (inches)</TableCell>
-              <TableCell>Weight (lbs)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {playerData.map((player) => (
-              <TableRow key={player.player_id}>
-                <TableCell>{player.player_name}</TableCell>
-                <TableCell>{new Date(player.birthdate).toLocaleDateString()}</TableCell>
-                <TableCell>{player.school}</TableCell>
-                <TableCell>{player.height_inch}</TableCell>
-                <TableCell>{player.weight_lbs}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+const LandingView = () => (
+  <div className="LandingView">
+    <div className="LandingView__hero">
+      <Container maxWidth="lg">
+        <div className="LandingView__hero">
+          <Typography variant="h2" component="h1" gutterBottom>
+            NBA Basketball Encyclopedia
+          </Typography>
+          <Typography variant="h5" paragraph>
+            Explore the world of NBA with detailed statistics and insights.
+          </Typography>
+        </div>
+      </Container>
     </div>
-  );
-};
+    <Container maxWidth="lg">
+      <div className="LandingView__featuresSection">
+        <Typography variant="h4" align="center" gutterBottom>
+          Key Features
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Link to="/player" style={{ textDecoration: 'none' }}>
+              <Card className="LandingView__featuresSection__featureCard">
+                <CardMedia
+                  className="LandingView__featuresSection__featureCardMedia"
+                  image="/assets/playerHeroComp.png"
+                  title="Player Statistics"
+                />
+                <CardContent className="LandingView__featuresSection__cardContent">
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Player Statistics
+                  </Typography>
+                  <Typography>
+                    Access comprehensive statistics for individual players, including performance metrics and salaries.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Link to="/team" style={{ textDecoration: 'none' }}>
+              <Card className="LandingView__featuresSection__featureCard">
+                <CardMedia
+                  className="LandingView__featuresSection__featureCardMedia"
+                  image="/assets/teamHeroComp.png"
+                  title="Team Statistics"
+                />
+                <CardContent className="LandingView__featuresSection__cardContent">
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Team Statistics
+                  </Typography>
+                  <Typography>
+                    Explore team-level statistics, including team performance, payroll, and historical data.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Link to="/highScoringGame" style={{ textDecoration: 'none' }}>
+              <Card className="LandingView__featuresSection__featureCard">
+                <CardMedia
+                  className="LandingView__featuresSection__featureCardMedia"
+                  image="/assets/gameHeroComp.png"
+                  title="Game Statistics"
+                />
+                <CardContent className="LandingView__featuresSection__cardContent">
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Game Statistics
+                  </Typography>
+                  <Typography>
+                    Dive into each games stats showing which teams played and the points scored.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  </div>
+);
 
 export default LandingView;
